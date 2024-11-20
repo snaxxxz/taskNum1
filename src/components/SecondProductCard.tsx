d46a447c3e790f5fe35767a8455ff8fbc5fd11a0
artur <@r1666s>
2024-11-20 16:33:27 +0300

    feat: block 2 created PP

---
+++ src/components/SecondProductCard.tsx
@@ -0,0 +1,123 @@
+import styles from './SecondProductCard.module.css'
+
+function SecondProductCard() {
+
+	return(
+		<>
+			<div className={styles.info__block}>
+				<div className={styles.firstInfo__block}>
+					<div className={styles.first_card__inBlock}>
+						<div className={styles.images__block}>
+							<img 
+							className={styles.mainImages} 
+							src="./public/images/img-desktop2.png" 
+							alt="" 
+							/>
+							<img 
+							className={styles.interactiveImages__block} 
+							src="./public/images/icon-fashion.png" 
+							alt="" 
+							/>
+						</div>
+						<div className={styles.textInfo__block}>
+							<div className={styles.first__textBlock}>
+								<div className={styles.link_block}>
+										<div className={styles.activelink}>
+												<a className={styles.itemLink} href="#">Fashion</a>
+										</div>
+										<div className={styles.links}>
+											<a className={styles.itemLink} href="#">Artículo</a>
+											<a className={styles.itemLink} href="#">Niños</a>
+											<a className={styles.itemLink} href="#">Proyecto</a>
+										</div>
+									</div>
+									<h4 className={styles.mainText__inBlock}>
+										¿Cómo hacer el cambio de carrera a Diseño de Productos 		Digitales?
+									</h4>
+							</div>
+							<div className={styles.second__textBlock}>
+								<p className={styles.text__block}>
+									¿Quieres saber qué se estudia para ser paisajista y formar 	parte de uno de los gremios que están en auge a nivel laboral? 	Sigue leyendo.
+								</p>
+								<div className={styles.dateAndTime__block}>
+									<div className={styles.dateBlock}>
+										<img src="./public/images/icon-calendar.png" alt="" />
+										<p className={styles.textDate}>2 de junio de 2022</p>
+									</div>
+									<div className={styles.timeBlock}>
+									<img src="./public/images/icon-watch.png" alt="" />
+									<p className={styles.textTime}>10 min</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className={styles.second_card__inBlock}>
+						<div className={styles.first_card__inBlock}>
+							<div className={styles.images__block}>
+								<img 
+								className={styles.mainImages} 
+								src="./public/images/img-desktop3.png" 
+								alt="" 
+								/>
+								<img 
+								className={styles.interactiveImages__block} 
+								src="./public/images/icon-diseno.png" 
+								alt="" 
+								/>
+							</div>
+							<div className={styles.textInfo__block}>
+								<div className={styles.first__textBlock}>
+									<div className={styles.link_block}>
+											<div className={styles.activelink}>
+													<a className={styles.itemLink} href="#">Diseño</a>
+											</div>
+											<div className={styles.links}>
+												<a className={styles.itemLink} href="#">Artículo</a>
+												<a className={styles.itemLink} href="#">Niños</a>
+												<a className={styles.itemLink} href="#">Proyecto</a>
+											</div>
+										</div>
+										<h4 className={styles.mainText__inBlock}>
+											¿Qué hace un paisajista y cuánto gana?
+										</h4>
+								</div>
+								<div className={styles.second__textBlock}>
+									<p className={styles.text__block}>
+										¿Quieres saber qué se estudia para ser paisajista y formar 	parte de uno de los gremios que están en auge a nivel 	laboral? Sigue leyendo.
+									</p>
+									<div className={styles.dateAndTime__block}>
+										<div className={styles.dateBlock}>
+											<img src="./public/images/icon-calendar.png" alt="" />
+											<p className={styles.textDate}>2 de junio de 2022</p>
+										</div>
+										<div className={styles.timeBlock}>
+										<img src="./public/images/icon-watch.png" alt="" />
+										<p className={styles.textTime}>10 min</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				{/* 3bq */}
+				<div className={styles.third_card__inBlock}>
+					<div className={styles.images__block}>
+						<img 
+						className={styles.mainImages} 
+						src="./public/images/img-desktop4.png" 
+						alt="" 
+						/>
+						<img 
+						className={styles.interactiveImages__block} 
+						src="./public/images/icon-diseno.png" 
+						alt="" 
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+export default SecondProductCard
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ProductCard from './components/ProductCard.tsx'
+import SecondProductCard from './components/SecondProductCard.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <ProductCard />
+    <SecondProductCard />
   </StrictMode>,
 )
